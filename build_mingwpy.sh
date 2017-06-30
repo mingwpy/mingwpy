7b2c64e9b53cf0e45861a587911d7b69bdd0f092
Matthew Brett <matthew.brett@gmail.com>
2017-06-30 21:20:46 +0100

    Test appveyor build

---
+++ build_mingwpy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script to build mingwpy on Appveyor systems
+# Expects (default)
+#  BITS: 64
+
+BITS=${BITS:-64}
+rm -rf build
+
+if [ "$BITS" == "64" ]; then
+    mw_arch=x86_64
+    mw_march="x86-64"
+    mw_exceptions=seh
+else
+    mw_arch=i686
+    mw_march=pentium4
+    mw_exceptions=sjlj
+fi
+
+pacman -Sy --noconfirm git svn zip tar autoconf make libtool automake p7zip \
+    patch bison gettext-devel wget sshpass texinfo
+pacman -Rs --noconfirm gcc gcc-fortran
+
+./build --mode=gcc-5.3.0 --static-gcc --arch=$mw_arg --march-x64="$mw_march" \
+    --mtune-x$BITS='generic' --rev=201603 --rt-version=trunk --threads=win32 \
+    --exceptions=$mw_exceptions --enable-languages=c,c++,fortran \
+    --buildroot=${PWD}/build --bootstrap --no-multilib --bin-compress
+ls build
